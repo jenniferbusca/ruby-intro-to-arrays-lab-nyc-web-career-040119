4d29d1ff4d1eb882e6f7a3db546ea95fced59320
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-28 12:03:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,11 +8,14 @@ def array_with_two_elements
   my_two_array = [1,2]
 end
 
-
 def first_element(array)
   return array.first
 end
 
 def third_element(array)
   return array[2]
+end
+
+def last_element(array)
+  return array.last
 end
